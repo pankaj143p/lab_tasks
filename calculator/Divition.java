3d1cbca5b8c9af2406f9677cb33c1bffe436e733
pankaj143p <pankaj114477pankaj@gmail.com>
2025-01-07 13:00:48 +0530

    substraction code added

---
+++ calculator/Divition.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Divition {
+   
+   public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = new String[2];
+        System.out.println("Enter first number: ");
+        inputs[0] = br.readLine();
+        System.out.println("Enter second number: ");
+        inputs[1] = br.readLine();
+        int a = Integer.parseInt(inputs[0]);
+        int b = Integer.parseInt(inputs[1]);
+        int c = add(a, b);
+        System.out.println("Division of " + a + " and " + b + " is " + c);
+    }
+    public static int add(int a, int b){
+        return a / b;
+    }
+    
+}
